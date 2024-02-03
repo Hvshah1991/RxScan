0c34a4dfa85e8292af93bb6e4f22de2e97a34d76
Harsh Shah <harshvshah.22@gmail.com>
2024-02-02 19:24:19 -0500

    selecting rendering added to reminders and cross-checked styling needs

--- src/components/Reminders/Reminders.js
+++ src/components/Reminders/Reminders.js
@@ -25,37 +25,44 @@ const Reminders = () => {
 
   return (
     <div className='reminders'>
-    <div className="reminders__border"></div>
-    <div className='reminders__cont-icon'>
-      <h2 className='reminders__title'>Reminders</h2>
-      <img
-            className="reminders__icon"
-            src={RemindersIcon}
-            alt="reminders-icon"
+      <div className="reminders__border"></div>
+      <div className='reminders__cont-icon'>
+        <h2 className='reminders__title'>Reminders</h2>
+        <img
+          className="reminders__icon"
+          src={RemindersIcon}
+          alt="reminders-icon"
         />
-    </div>
-      <ul className='reminders__main'>
-        {reminders.map((reminder, index) => {
-          return (
-            <li className='reminders__sub' key={index}>
-              <strong className='reminders__med'>Medication:</strong> {reminder.medications}, 
-              <strong className='reminders__date'>Date:</strong> {reminder.date instanceof Date ? reminder.date.toLocaleDateString() : 'Invalid Date'},
-              <strong className='reminders__time'>Time:</strong> {reminder.time.hour}:{reminder.time.minute}, 
-              <strong className='reminders__freq'>Frequency:</strong> {reminder.frequency}
-              {reminder.days && (
-                <div className='reminders__days-cont'>
-                  <strong className='reminders__days'>Days:</strong> {reminder.days.join(', ')}
-                </div>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-      <div className='reminders__cont'>
-        <button className='reminders__button' onClick={clearReminders}>Clear Reminders</button>
       </div>
+      {reminders.length === 0 ? (
+        <p className="reminders__no-reminders">Currently No Reminders</p>
+      ) : (
+        <>
+          <ul className='reminders__main'>
+            {reminders.map((reminder, index) => {
+              return (
+                <li className='reminders__sub' key={index}>
+                  <strong className='reminders__med'>Medication:</strong> {reminder.medications}, 
+                  <strong className='reminders__date'>Date:</strong> {reminder.date instanceof Date ? reminder.date.toLocaleDateString() : 'Invalid Date'},
+                  <strong className='reminders__time'>Time:</strong> {reminder.time.hour}:{reminder.time.minute}, 
+                  <strong className='reminders__freq'>Frequency:</strong> {reminder.frequency}
+                  {reminder.days && (
+                    <div className='reminders__days-cont'>
+                      <strong className='reminders__days'>Days:</strong> {reminder.days.join(', ')}
+                    </div>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+          <div className='reminders__cont'>
+            <button className='reminders__button' onClick={clearReminders}>Clear Reminders</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
+
 
 export default Reminders;
